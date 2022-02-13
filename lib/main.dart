713faa09d24b963f2da7011738c63614c7a77fe3
Jaisonemathew <90517885+Jaisonemathew@users.noreply.github.com>
2022-02-13 16:34:56 +0530

    Completed app

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,50 @@
+import 'dart:math';
 import 'package:flutter/material.dart';
 
 void main() => runApp(
       MaterialApp(
-        home: Scaffold(),
+        debugShowCheckedModeBanner: false,
+        home: Scaffold(
+          backgroundColor: Colors.white,
+          appBar: AppBar(
+            title:Text('Ask me anything'),
+            backgroundColor: Colors.black,
+          ),
+          body: askmeball(),
+        ),
       ),
     );
+
+class askmeball extends StatefulWidget {
+  @override
+  _askmeballState createState() => _askmeballState();
+}
+
+class _askmeballState extends State<askmeball> {
+  int ballnumber=1;
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Expanded(
+        child :TextButton(
+          child:Image.asset(
+            'images/ball$ballnumber.png'
+          ),
+          onPressed:()
+            {
+              setState(() {
+                ballnumber = Random().nextInt(4) + 1;
+              });
+            }
+        ),
+      ),
+    );
+  }
+}
+
+
+
+
+
+
+
